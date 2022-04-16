7730bb67cdf4fef053981e703a1d936f75d0895d
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2022-04-16 15:09:11 +0300

    used an observable to make an api request in user service

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  getUsers(): Observable<any[]> {
+    //let param = new HttpParams();
+    //param = param.append('username', 'SamTomashi');
+    return this.httpClient.get<any[]>(`https://api.github.com/users/samtomashi/repos`, {
+      //params: param,
+      // headers : this.headers,
+      withCredentials: true
+    });
+   }
+  
+
+
 }
