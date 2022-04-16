f9c1358d1d0dd2e803bc8f79ea29461743f14da2
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2022-04-16 19:17:37 +0300

    added baseurl to the environment file

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/users.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  title: string = "Github Search API"
 
-  constructor() { }
+  constructor(private usersService:UsersService) { }
+
+
+  getPublicRepositories(){
+    this.usersService.getUsers().subscribe((response)=>{
+      console.log(response) 
+    })
+  }
 
   ngOnInit(): void {
+    this.getPublicRepositories()
   }
 
 }
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,22 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  private baseUrl = environment.BASE_URL
 
   constructor(private httpClient:HttpClient) { }
 
   getUsers(): Observable<any[]> {
     //let param = new HttpParams();
     //param = param.append('username', 'SamTomashi');
-    return this.httpClient.get<any[]>(`https://api.github.com/users/samtomashi/repos`, {
+    return this.httpClient.get<any[]>(`${this.baseUrl}/users/samtomashi/repos`, {
       //params: param,
       // headers : this.headers,
-      withCredentials: true
+      //withCredentials: true
     });
    }
   
