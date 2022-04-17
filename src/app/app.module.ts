72c7076667ac28d59702019b1d019308c08792da
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2022-04-17 10:43:47 +0300

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,24 @@ import { AppComponent } from './app.component';
 import { UsersService } from './users.service';
 import { HomeComponent } from './components/home/home.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: '', redirectTo:'home', pathMatch:'full'},
+      {path: '**', redirectTo:'home', pathMatch:'full'}
+    ]),
   ],
   providers: [UsersService],
   bootstrap: [AppComponent]
